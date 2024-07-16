ebad8bf88d2ab8e5bc480886aef4137ce2343eb1
Bhawuk Arora <bhawukarora042@gmail.com>
2024-07-17 01:11:50 +0530

    Updated 02ServletTest

--- 02ServletTest/src/main/java/com/springlearning/App.java
+++ 02ServletTest/src/main/java/com/springlearning/App.java
@@ -1,13 +1,27 @@
 package com.springlearning;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.apache.catalina.Context;
+import org.apache.catalina.startup.Tomcat;
+
+public class App {
+    public static void main(String[] args) {
+        try {
+            // System.out.println("Hello World!");
+            Tomcat tomcat = new Tomcat();
+            tomcat.setPort(9080);
+
+            // Add the root context
+            Context context = tomcat.addContext("", null);
+
+            // Add the HelloServlet to the context
+            Tomcat.addServlet(context, "HelloServlet", new HelloServlet());
+            context.addServletMappingDecoded("/hello", "HelloServlet");
+
+            // Start the Tomcat server
+            tomcat.start();
+            tomcat.getServer().await();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
--- 02ServletTest/src/main/java/com/springlearning/HelloServlet.java
+++ 02ServletTest/src/main/java/com/springlearning/HelloServlet.java
@@ -1,5 +1,25 @@
-package main.java.com.springlearning;
+package com.springlearning;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public class HelloServlet extends HttpServlet {
-    
+    public void service(HttpServletRequest req, HttpServletResponse res) {
+        System.out.println("In Sevice");
+
+        res.setContentType("text/html");
+        PrintWriter out;
+        try {
+            out = res.getWriter();
+            out.println("<h1>Hallo Bhawuk!</h1>");
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        
+
+    }
 }
